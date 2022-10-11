c74de7a16d6da2fa2719c3f893e0262c5b5feafb
sushil-rgb <sushil.bhandari002@gmail.com>
2022-10-12 03:20:50 +0545

    minor repush

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tools import JbHiFi
 import pandas as pd
 
 
-jbhifi_url = "https://www.jbhifi.com.au/collections/headphones-speakers-audio/soundbars"
+jbhifi_url = "https://www.jbhifi.com.au/collections/tvs/wall-brackets"
 
 jbhifi = JbHiFi(jbhifi_url).allProductLinks()
 
